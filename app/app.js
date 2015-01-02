88696784bb6c16336cc7e0763ee918e7d6377665
unbelt <unbelt@outlook.com>
2015-01-03 00:35:29 +0200

    HTML pages for ads app refactored

--- app/app.js
+++ app/app.js
@@ -6,6 +6,27 @@ var app = angular.module('adsApp', ['ngRoute', 'ngResource'])
             .when('/home', {
                 templateUrl: 'app/views/home.html'
             })
+            .when('/login', {
+                templateUrl: 'app/views/login.html'
+            })
+            .when('/register', {
+                templateUrl: 'app/views/register.html'
+            })
+            .when('/my-ads', {
+                templateUrl: 'app/views/user/catalog/show.html'
+            })
+            .when('/create-ad', {
+                templateUrl: 'app/views/user/catalog/create.html'
+            })
+            .when('/edit-ad', {
+                templateUrl: 'app/views/user/catalog/edit.html'
+            })
+            .when('/delete-ad', {
+                templateUrl: 'app/views/user/catalog/delete.html'
+            })
+            .when('/edit-profile', {
+                templateUrl: 'app/views/user/edit.html'
+            }) // *************************************************** Admin Area
             .otherwise({redirectTo: '/home'});
     }
 );
