65183e440701e02533a673d5ba185140b7e64f2d
unbelt <unbelt@outlook.com>
2015-01-02 23:10:23 +0200

    Web Server; Build System and initial JS and CSS libraries added

---
+++ app.build.js
@@ -0,0 +1,8 @@
+({
+    appDir: './',
+    baseUrl: 'public/js',
+    dir: 'dist',
+    name: 'main',
+    mainConfigFile: 'public/js/main.js',
+    optimizeCss: 'standard'
+})
---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+require.config({
+    paths: {
+        'jquery': '../vendor/jquery/dist/jquery',
+        'bootstrap': '../vendor/bootstrap/dist/js/bootstrap'
+    },
+    shim: {
+        'bootstrap': ['jquery']
+    }
+});
+
+require(['bootstrap'], function () {
+    $(function () {
+        'use strict';
+
+        // Page show-up
+        $('#status').fadeOut();
+        $('#preloader').delay(150).fadeOut('slow');
+        $('body').delay(150).css({'overflow': 'visible'});
+
+        // target _blank to all external links
+        $('body a').each(function () {
+            if (this.href.indexOf(location.hostname) === -1) {
+                $(this).attr('target', '_blank');
+            }
+        });
+    }());
+});
